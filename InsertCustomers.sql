b3c1038c0f60e164464c1c99f9f4a116d01779dd
sfc-gh-hsrinivasan <HARI.SRINIVASAN@UNITEUS.COM>
2025-08-13 22:27:11 +0000

    Added a new row

--- InsertCustomers.sql
+++ InsertCustomers.sql
@@ -4,4 +4,5 @@ VALUES
     ('Brian', 'Baker', 'brian.baker@example.com', '555-0102', CURRENT_TIMESTAMP),
     ('Cathy', 'Clark', 'cathy.clark@example.com', '555-0103', CURRENT_TIMESTAMP),
     ('David', 'Dixon', 'david.dixon@example.com', '555-0104', CURRENT_TIMESTAMP),
-    ('Eva', 'Edwards', 'eva.edwards@example.com', '555-0105', CURRENT_TIMESTAMP);
+    ('Eva', 'Edwards', 'eva.edwards@example.com', '555-0105', CURRENT_TIMESTAMP)
+    ('Tom', 'Brady', 'tom.brady@example.com', '555-0700', CURRENT_TIMESTAMP);
